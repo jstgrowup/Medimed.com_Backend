53ccd0a2dbc14ee79f37bc7e8f421c3eb38630cd
jstgrowup <deysubham999@gmail.com>
2022-11-19 22:58:40 +0530

    error handling

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const Razorpay = require("razorpay");
 const ProductModel = require("./models/Product.model");
 app.get("/search", async (req, res) => {
   try {
-    const { title } = req.query
+    const { title } = req.query;
 
     const agg = [
       {
@@ -25,77 +25,79 @@ app.get("/search", async (req, res) => {
             query: title,
             path: "title",
             fuzzy: {
-              maxEdits: 2
-            }
-          }
-        }
-      }, {
-        $limit: 8
-      }, {
+              maxEdits: 2,
+            },
+          },
+        },
+      },
+      {
+        $limit: 8,
+      },
+      {
         $project: {
           _id: 1,
           title: 1,
           price: 1,
           url: 1,
+        },
+      },
+    ];
+    const arr = await ProductModel.aggregate(agg);
 
-
-        }
-      }
-    ]
-    const arr = await ProductModel.aggregate(agg)
-
-    res.json(arr)
-  } catch (error) {
-    console.log(error);
-  }
-})
+    res.json(arr);
+  } catch (error) {}
+});
 app.post("/order", async (req, res) => {
-  const { amount } = req.body
-  console.log(amount)
+  const { amount } = req.body;
+  console.log(amount);
   try {
-    var instance = new Razorpay({ key_id: process.env.RAZORPAY_KEY_ID, key_secret: process.env.RAZORPAY_KEY_SECRET })
+    var instance = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
     let order = await instance.orders.create({
       amount: amount,
       currency: "INR",
       receipt: "receipt#1",
-    })
-    return res.status(201).send({ message: "Successful", order: order, amount: amount })
+    });
+    return res
+      .status(201)
+      .send({ message: "Successful", order: order, amount: amount });
   } catch (e) {
-    return res.status(401).send({ message: e.message })
+    return res.status(401).send({ message: e.message });
   }
-})
+});
 app.post("/getuser", async (req, res) => {
   const { userid } = req.body;
   if (userid) {
     const data = await UserModel.findOne({ userid: userid });
     res.send(data);
-  }
-  else {
+  } else {
     res.status(400).send({
-      message: "Please provide a userid"
+      message: "Please provide a userid",
     });
   }
 });
 app.post("/loginuser", async (req, res) => {
   const { email, password } = req.body;
 
-
-  if (email, password) {
+  if ((email, password)) {
     try {
-      const data = await UserModel.findOne({ email: email, password: password });
+      const data = await UserModel.findOne({
+        email: email,
+        password: password,
+      });
       if (!data) {
-        return res.status(404).send("no")
+        return res.status(404).send("no");
       }
 
       res.send(data);
-
     } catch (error) {
       res.status(500).send("wrong credentials");
     }
-  }
-  else {
+  } else {
     res.status(400).send({
-      message: "Please provide a userid"
+      message: "Please provide a userid",
     });
   }
 });
@@ -104,8 +106,7 @@ app.post("/postUserViaForm", async (req, res) => {
   if (userid) {
     const data = await UserModel.create(req.body);
     res.send(data);
-  }
-  else {
+  } else {
     res.status(400).send("invalid userid");
   }
 });
